ff6b88fe04fb7b87ab53d0bcd4b9524f7df38c4d
rogutier <60100142+rogutier@users.noreply.github.com>
2023-06-19 03:39:10 +0000

    gitignore add

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'rogutier',
+    'PASSWORD': 'S8zLq5pERMrx',
+    'HOST': 'ep-white-cake-232061.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
